aecb8d05ce9fa4ffc9d50bd90ea7aeecefe579f6
Lucas Figueiredo <lukas.liberato14@gmail.com>
2021-09-02 00:39:46 -0300

    Projeto 2 - Primeiro commit

---
+++ src/main/java/com/digitalinnovation/one/PersonAPI_bootcamp/PersonApiBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.PersonAPI_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/one/PersonAPI_bootcamp/PersonApiBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.PersonAPI_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
