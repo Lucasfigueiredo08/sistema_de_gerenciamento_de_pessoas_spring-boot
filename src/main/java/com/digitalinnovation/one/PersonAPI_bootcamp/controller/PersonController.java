ed675081f716638bad104c56f224b90ff5d43f52
Lucas Figueiredo <lukas.liberato14@gmail.com>
2021-09-02 02:21:28 -0300

    Listar usuarios

--- src/main/java/com/digitalinnovation/one/PersonAPI_bootcamp/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/one/PersonAPI_bootcamp/controller/PersonController.java
@@ -5,11 +5,13 @@ import com.digitalinnovation.one.PersonAPI_bootcamp.dto.response.MessageResponse
 import com.digitalinnovation.one.PersonAPI_bootcamp.entity.Person;
 import com.digitalinnovation.one.PersonAPI_bootcamp.repository.PersonRepository;
 import com.digitalinnovation.one.PersonAPI_bootcamp.service.PersonService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -26,5 +28,10 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 }
